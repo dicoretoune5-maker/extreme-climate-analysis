920dd6587dd5dace571edec7ddda9e4239cd3a96
Dicson <dicoretoune5@gmail.com>
2026-09-04 01:59:38 +0200

    j'ai supprimé les date manque parce qu’on ne peut pas extraire l’année si la date est vide. - cette ligne cherche une année dans la colonne Date. - j'ai extraire la colonne event type parce qu'il contient déjà le type événement

--- analyse.py
+++ analyse.py
@@ -13,3 +13,48 @@ print(df.info())
 # Vérifier les valeurs manquantes
 print("\nVérification des valeurs manquantes :")
 print(df.isnull().sum())
+# Supprimer les lignes sans date
+df_clean = df.dropna(subset=["Date"]).copy()
+# Extraire l'année de début
+df_clean["annee_debut"] = df_clean["Date"].str.extract(r'(\d{4})')
+# Créer une colonne mois vide car le fichier ne contient pas le mois
+df_clean["mois"] = None
+# Renommer le type d'événement
+df_clean["type_evenement"] = df_clean["Event Type"]
+# Créer une colonne région approximative depuis le titre
+df_clean["region"] = df_clean["Title"].str.replace(r",?\s*\d{4}.*", "", regex=True)
+# Créer les colonnes absentes dans le fichier original
+df_clean["blesses"] = None
+df_clean["morts"] = None
+df_clean["degats_materiels"] = None
+print("\nDonnées nettoyées :")
+print(df_clean[[
+    "annee_debut",
+    "mois",
+    "region",
+    "type_evenement",
+    "blesses",
+    "morts",
+    "degats_materiels"
+]].head())
+# 3. Agrégation temporelle 
+# Supprimer les lignes sans date, car on ne peur pas le placer dans le temps
+df_clean = df.dropna(subset=["Date"]).copy()
+# Agréger les données par année la colonne Date
+df_clean["annee"] = df_clean["Date"].str.extract(r"(\d{4})")
+# Convertir l'année en nombre
+df_clean["annee"] = df_clean["annee"].astype(int)
+# Le fichier contient pas le mois, donc on crée une colonne mois inconnue
+df_clean["mois"] = "inconnu"
+# Renommer le type d'événement pour avoir un nom plus simple
+df_clean["type_evenement"] = df_clean["Event Type"]
+# Compter les événements par année mois et catégorie
+events_par_temps = (
+    df_clean
+    .groupby(["annee", "mois", "type_evenement"])
+    .size()
+    .reset_index(name="nombre_evenements")
+)
+
+print("\nNombre d'événements par année, mois et catégorie :")
+print(events_par_temps.head(20))
